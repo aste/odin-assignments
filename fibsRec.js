fbbc51216111460c70ce5bd6a455f0902a640f61
Aste <aste@tuta.io>
2023-10-31 20:33:56 +0100

    Add Fibonacci recursive and Merge Sort recursive implementations

---
+++ fibsRec.js
@@ -0,0 +1,5 @@
+const fibsRec= (n) => n < 2 ? n : (fibsRec(n - 1) + fibsRec(n - 2))
+
+console.log(fibsRec(13))
+
+
---
+++ mergeSort.js
@@ -0,0 +1,37 @@
+const merge = (leftArr, rightArr) => {
+    const output = []
+    let leftIndex = 0
+    let rightIndex = 0
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+
+        if (leftArr[leftIndex] < rightArr[rightIndex]) {
+            output.push(leftArr[leftIndex])
+            leftIndex++
+        } else {
+            output.push(rightArr[rightIndex])
+            rightIndex++
+        }
+    }
+
+    return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+}
+
+const mergeSort = arr => {
+    if (arr.length <= 1) {
+        return arr
+    }
+    console.log(arr)
+    console.log(typeof arr)
+
+    const middleIndex = Math.floor(arr.length / 2)
+    const leftArr = arr.slice(0, middleIndex)
+    const rightArr = arr.slice(middleIndex)
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr))
+}
+
+
+let testArr = [101, 24, 1, 9, 3, 5, 2, 28, 132, 3, 3, 48,]
+
+console.log(mergeSort(testArr))
